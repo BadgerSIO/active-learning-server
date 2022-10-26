376ee4f29a45c9da5185633ac4ebfc1389d2a521
BadgerSIO <saadibneomar@gmail.com>
2022-10-26 10:50:51 +0600

    course json and courseDetails json added

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const courses = require("./data/courses.json");
+const coursesDetail = require("./data/coursesWithDetail.json");
+
+app.get("/", (req, res) => {
+  res.send(courses);
+});
+
+app.listen(port, () => {
+  console.log("Active learning server is running on port ", port);
+});
